43b3c6d755d7727b71092c7c9846b4e935a134da
zenoyang <cookie.yz@qq.com>
2021-12-16 17:05:39 +0800

    [Storage Layer] Support bloom filter predicate, add UT

--- be/test/olap/bloom_filter_column_predicate_test.cpp
+++ be/test/olap/bloom_filter_column_predicate_test.cpp
@@ -28,6 +28,10 @@
 #include "runtime/string_value.hpp"
 #include "runtime/vectorized_row_batch.h"
 #include "util/logging.h"
+#include "vec/columns/column_nullable.h"
+#include "vec/core/block.h"
+
+using namespace doris::vectorized;
 
 namespace doris {
 
@@ -171,6 +175,49 @@ TEST_F(TestBloomFilterColumnPredicate, FLOAT_COLUMN) {
     pred->evaluate(&col_block, _row_block->selection_vector(), &select_size);
     ASSERT_EQ(select_size, 1);
     ASSERT_FLOAT_EQ(*(float*)col_block.cell(_row_block->selection_vector()[0]).cell_ptr(), 5.1);
+
+    // for vectorized::Block no null
+    col_block_view = ColumnBlockView(&col_block);
+    for (int i = 0; i < size; ++i, col_block_view.advance(1)) {
+        col_block_view.set_null_bits(1, false);
+        *reinterpret_cast<float*>(col_block_view.data()) = i + 0.1f;
+    }
+    _row_block->clear();
+    auto vec_sel_size = _row_block->selected_size();
+    vectorized::Block vec_block = tablet_schema.create_block(return_columns);
+    _row_block->convert_to_vec_block(&vec_block);
+    ColumnPtr vec_col = vec_block.get_columns()[0];
+    pred->evaluate(const_cast<doris::vectorized::IColumn&>(*vec_col),
+                   _row_block->selection_vector(), &vec_sel_size);
+    ASSERT_EQ(vec_sel_size, 3);
+    auto* nullable = check_and_get_column<ColumnNullable>(*vec_col);
+    auto* vec =
+            check_and_get_column<vectorized::ColumnVector<float>>(nullable->get_nested_column());
+    ASSERT_FLOAT_EQ((vec->get_element(_row_block->selection_vector()[0])), 4.1);
+    ASSERT_FLOAT_EQ((vec->get_element(_row_block->selection_vector()[1])), 5.1);
+    ASSERT_FLOAT_EQ((vec->get_element(_row_block->selection_vector()[2])), 6.1);
+
+    // for vectorized::Block has nulls
+    col_block_view = ColumnBlockView(&col_block);
+    for (int i = 0; i < size; ++i, col_block_view.advance(1)) {
+        if (i % 2 == 0) {
+            col_block_view.set_null_bits(1, true);
+        } else {
+            col_block_view.set_null_bits(1, false);
+            *reinterpret_cast<float*>(col_block_view.data()) = i + 0.1;
+        }
+    }
+    _row_block->clear();
+    vec_sel_size = _row_block->selected_size();
+    vec_block = tablet_schema.create_block(return_columns);
+    _row_block->convert_to_vec_block(&vec_block);
+    vec_col = vec_block.get_columns()[0];
+    pred->evaluate(const_cast<doris::vectorized::IColumn&>(*vec_col),
+                   _row_block->selection_vector(), &vec_sel_size);
+    ASSERT_EQ(vec_sel_size, 1);
+    nullable = check_and_get_column<ColumnNullable>(*vec_col);
+    vec = check_and_get_column<vectorized::ColumnVector<float>>(nullable->get_nested_column());
+    ASSERT_FLOAT_EQ((vec->get_element(_row_block->selection_vector()[0])), 5.1);
 
     delete pred;
 }
